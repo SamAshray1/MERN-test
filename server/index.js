439a2d057ce7fda79d80e49b2bb2a80bd85b380e
SamAshray1 <56475424+SamAshray1@users.noreply.github.com>
2023-04-05 09:51:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,9 +6,17 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
+if (true) {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "client", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
   
   app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
